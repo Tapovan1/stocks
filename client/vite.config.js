574364d0672a46b400764e2332ff57fb19cb4c87
Patel Swapnil <info.tapovan2@gmail.com>
2025-06-08 17:36:31 +0530

    fix server allow

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  server: {
+    host: '0.0.0.0', // required for external access
+    port: 5173,
+    allowedHosts: ['stocks.tapovanvidhyamandirsankul.com'], // 👈 add your domain here
+  },
   
   plugins: [
     react(),
