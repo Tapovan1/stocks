d191e381c975010d6fc246d9f8161403ef876876
Patel Swapnil <info.tapovan2@gmail.com>
2025-06-09 08:10:11 +0530

    fix server allow

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,13 +4,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // required for Docker / external access
+    host: '0.0.0.0', // Docker external access
     port: 5173,
     allowedHosts: ['stocks.tapovanvidhyamandirsankul.com'],
     proxy: {
       '/api': {
-        target: 'stocks.tapovanvidhyamandirsankul.com',
+        target: 'http://server:5000',  // or 'http://localhost:5000' if running locally outside Docker
         changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '/api'), // usually optional, but explicit
       },
     },
   },
